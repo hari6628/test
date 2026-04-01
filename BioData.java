fa61f7d3d8ce8e593e3eca442d7896f0b9b0ed39
hari6628 <you@example.com>
2026-04-01 11:51:13 +0530

    Classwork

---
+++ BioData.java
@@ -0,0 +1,21 @@
+class BioData
+{
+public static void main (String[] args)
+{
+	String name="Hari";
+	String lastname="Narayanan";
+	char gender='M';
+	long mobilenumber =	123456789l;
+	float height =5.7f;
+	double balance=10000;
+	boolean love =false ;
+	
+	System.out.print(name +" "+ lastname);
+	System.out.println(" My age :" + gender);
+	System.out.println("my phone number :" +mobilenumber);
+	System.out.println("Height :" +height);
+	System.out.println("Account balance :"+balance);
+	System.out.println(love);
+	
+}
+}
